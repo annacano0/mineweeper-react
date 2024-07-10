947a3c4a95914018bc2a47f5204eaf35cce9a048
anna <anna.elisabet5@gmail.com>
2024-07-10 15:28:03 +0200

    remove unused code add Mines

--- minesweeper/src/components/Board.jsx
+++ minesweeper/src/components/Board.jsx
@@ -2,33 +2,25 @@
 import Cell from './Cell.jsx';
 import { useState } from 'react';
 
-function addMinesToBoard(board){// TODO: implement this with random 0 or 1
+function addMinesToBoard(board, mines){// TODO: implement this with random 0 or 1
     return board
 }
 
 function generateBoard(rows, columns, mines) { //TODO: modify how mines are added
   let emptyBoard = [];
-  let minesLeftToAdd = mines
   for (let i = 0; i <= rows - 1; i++) {
     let newRow = [];
     for (let j = 0; j <= columns - 1; j++) {
-      let addMineToCell = 0
-      if(minesLeftToAdd>0){
-        addMineToCell=Math.floor(Math.random() * 2)
-        if(addMineToCell==1) minesLeftToAdd--
-        console.log(addMineToCell)  
-      } 
-        
       let newCell = {
         coordinates: [i,j],
-        hasMine: addMineToCell,
+        hasMine: false,
         adjacentMines: 0,
       };
       newRow.push(newCell);
     }
     emptyBoard.push(newRow);
   }
-  let boardWithMines= addMinesToBoard(emptyBoard)
+  let boardWithMines= addMinesToBoard(emptyBoard, mines)
   return boardWithMines;
 }
 
