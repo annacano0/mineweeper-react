1f7f125ed7b2b914b6ba064393778c36f3eb64d0
anna <anna.elisabet5@gmail.com>
2024-07-10 13:42:18 +0200

    fix events on cell

--- minesweeper/src/components/Cell.jsx
+++ minesweeper/src/components/Cell.jsx
@@ -1,22 +1,28 @@
 'use client'
 import { useState } from 'react';
 
-function handleLeftClick(event){
-    console.log("reveal cell")
-}
-
-function handleRightClick(event){
-    event.preventDefault();
-    console.log('flag / unflag cell')
-}
-
 export default function Cell({cell}) {
     const [isUncovered, setIsUncovered]= useState(false);
-    const [isTagged, setIsTagged]= useState(false);
+    const [isTagged, setIsFlagged]= useState(false);
     const hasMine=cell.hasMine
+
+    const handleFlag = (event) => {
+        event.preventDefault();
+        if(isUncovered) setIsFlagged(false)
+        else setIsFlagged(true)
+        console.log('flag / unflag cell')
+    }
+
+    const handleReveal = () => {
+        setIsUncovered(true)
+        console.log("reveal cell")
+    }
+
     return (
-        <section className="cell" onClick={handleLeftClick} onContextMenu={(event)=>handleRightClick(event)} >
-            <p>{cell.coordinates[0]+"," +cell.coordinates[1]}</p>
+        <section className="cell" onClick={handleReveal} onContextMenu={(event)=>handleFlag(event)} >
+            {<p>{cell.coordinates[0]+"," +cell.coordinates[1]}</p>}
         </section>
     ); 
+
+    
 }
