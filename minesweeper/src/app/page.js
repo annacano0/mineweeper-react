daa7f21edc0ba5b966e6a7be7836eb1083873e26
anna <anna.elisabet5@gmail.com>
2024-07-12 16:38:13 +0200

    add css, background and others

--- minesweeper/src/app/page.js
+++ minesweeper/src/app/page.js
@@ -4,7 +4,7 @@ import Board from "../components/Board.jsx"
 export default function Home() {
   return (
     <>
-      <h1>Minesweeper</h1>
+    <header className="game-header"><h1>Minesweeper</h1></header>
       <Board rows={3} columns={4} mines={2}/>
     </>
   );
